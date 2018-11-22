6e01c37f4a7239a7ae0e87a6be83a4cfdcbd5855
Layon Martins <layonmartins03@gmail.com>
2018-11-22 10:44:52 -0200

    create custom adapter class

--- app/src/main/java/com/example/android/listview/MainActivity.java
+++ app/src/main/java/com/example/android/listview/MainActivity.java
@@ -2,12 +2,40 @@ package com.example.android.listview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    String[] nameArray = {"Octopus", "Pig", "Sheep", "Rabbit", "Snake", "Spider"};
+
+    String[] infoArray = {
+            "8 tentacled monster",
+            "Delicious in rolls",
+            "Great for jumpers",
+            "Nice in a stew",
+            "Great for shoes",
+            "Scary."
+    };
+
+    Integer[] imageArray = {
+            R.drawable.octopus,
+            R.drawable.pig,
+            R.drawable.sheep,
+            R.drawable.rabbit,
+            R.drawable.snake,
+            R.drawable.spider
+    };
+
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        CustomListAdapter whatever = new CustomListAdapter(this, nameArray, infoArray, imageArray);
+
+        listView = (ListView) findViewById(R.id.listviewID);
+        listView.setAdapter(whatever);
     }
 }
